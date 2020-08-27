c2cc295c1498ac1957e9ddda1d4dc3d8fdade13c
Erik Schierboom <erik_schierboom@hotmail.com>
2020-08-27 11:02:27 +0200

    Add tests

--- test/worker_test.rb
+++ test/worker_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 module ToolingInvoker
   class WorkerTest < Minitest::Test
-    def test_flow
+    def test_flow_for_test_runner
       job_id = "123"
       language = "ruby"
       exercise = "bob"
@@ -24,6 +24,90 @@ module ToolingInvoker
       job = mock(id: job_id, to_h: results)
 
       TestRunnerJob.expects(:new).with(
+        job_id, language, exercise, s3_uri, container_version, execution_timeout
+      ).returns(job)
+      InvokeRunc.expects(:call).with(job)
+      RestClient.expects(:get).
+        with("#{config.orchestrator_address}/jobs/next").
+        returns(mock(body: resp.to_json))
+
+      RestClient.expects(:patch).
+        with(
+          "#{config.orchestrator_address}/jobs/#{job_id}",
+          results
+        )
+
+      service = Worker.new
+      service.expects(:loop).yields
+      service.expects(:sleep).never
+
+      service.()
+    end
+
+    def test_flow_for_representer
+      job_id = "123"
+      language = "ruby"
+      exercise = "bob"
+      s3_uri = "s3://..."
+      container_version = "v1"
+      execution_timeout = 10
+
+      resp = {
+        type: "representer",
+        id: job_id,
+        language: language,
+        exercise: exercise,
+        s3_uri: s3_uri,
+        container_version: container_version,
+        execution_timeout: execution_timeout
+      }
+
+      results = mock
+      job = mock(id: job_id, to_h: results)
+
+      RepresenterJob.expects(:new).with(
+        job_id, language, exercise, s3_uri, container_version, execution_timeout
+      ).returns(job)
+      InvokeRunc.expects(:call).with(job)
+      RestClient.expects(:get).
+        with("#{config.orchestrator_address}/jobs/next").
+        returns(mock(body: resp.to_json))
+
+      RestClient.expects(:patch).
+        with(
+          "#{config.orchestrator_address}/jobs/#{job_id}",
+          results
+        )
+
+      service = Worker.new
+      service.expects(:loop).yields
+      service.expects(:sleep).never
+
+      service.()
+    end
+
+    def test_flow_for_analyzer
+      job_id = "123"
+      language = "ruby"
+      exercise = "bob"
+      s3_uri = "s3://..."
+      container_version = "v1"
+      execution_timeout = 10
+
+      resp = {
+        type: "analyzer",
+        id: job_id,
+        language: language,
+        exercise: exercise,
+        s3_uri: s3_uri,
+        container_version: container_version,
+        execution_timeout: execution_timeout
+      }
+
+      results = mock
+      job = mock(id: job_id, to_h: results)
+
+      AnalyzerJob.expects(:new).with(
         job_id, language, exercise, s3_uri, container_version, execution_timeout
       ).returns(job)
       InvokeRunc.expects(:call).with(job)
