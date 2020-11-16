6995637dfe2acb6cb18bfe3503d5893cf06e20b5
Jeremy Walker <jez.walker@gmail.com>
2020-11-16 19:09:22 +0000

    Fix the tests

--- lib/tooling_invoker/configuration.rb
+++ lib/tooling_invoker/configuration.rb
@@ -28,7 +28,7 @@ module ToolingInvoker
     end
 
     def job_polling_delay
-      (ENV["JOB_POLLING_DELAY"] || 0.1).to_f
+      (ENV["JOB_POLLING_DELAY"] || 1).to_f
     end
 
     def jobs_efs_dir
