2dafbb2688e8c80cb15d71a48d9b331489444953
Jeremy Walker <jez.walker@gmail.com>
2021-09-28 14:51:46 +0100

    Run in parallel

--- lib/tooling_invoker/upload_metadata.rb
+++ lib/tooling_invoker/upload_metadata.rb
@@ -8,13 +8,19 @@ module ToolingInvoker
       # The server can't access redis (deliberately)
       # so we can't retrieve the job, but we don't actually
       # need to as we're just using the key to write to S3.
-      helper = Exercism::ToolingJob.new(job.id, {})
-      helper.store_stdout!(job.stdout)
-      helper.store_stderr!(job.stderr)
-      helper.store_metadata!(metadata)
+      # We write these in parallel to save time.
+      [
+        Thread.new { build_helper.store_stdout!(job.stdout) },
+        Thread.new { build_helper.store_stderr!(job.stderr) },
+        Thread.new { build_helper.store_metadata!(metadata) }
+      ].each(&:join)
     end
 
     private
+    def build_helper
+      Exercism::ToolingJob.new(job.id, {})
+    end
+
     def metadata
       {
         id: job.id,
