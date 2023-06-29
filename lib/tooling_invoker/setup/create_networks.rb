9bfa81027af54c13692ac430e76df51cef687c0e
Jeremy Walker <jez.walker@gmail.com>
2023-06-29 16:34:02 +0100

    Fix namespace issue

--- lib/tooling_invoker/setup/create_networks.rb
+++ lib/tooling_invoker/setup/create_networks.rb
@@ -1,15 +1,17 @@
-module ToolingInvoker::Setup
-  class CreateNetworks
-    include Mandate
+module ToolingInvoker
+  module Setup
+    class CreateNetworks
+      include Mandate
 
-    def call
-      # Setup docker network. If the network already
-      # exists then this will be a noop. It takes about
-      # 120ms to exec, so just do it on worker init
-      system(
-        "docker network create --internal internal",
-        out: File::NULL, err: File::NULL
-      )
+      def call
+        # Setup docker network. If the network already
+        # exists then this will be a noop. It takes about
+        # 120ms to exec, so just do it on worker init
+        system(
+          "docker network create --internal internal",
+          out: File::NULL, err: File::NULL
+        )
+      end
     end
   end
 end
--- lib/tooling_invoker/setup/wait_for_manager.rb
+++ lib/tooling_invoker/setup/wait_for_manager.rb
@@ -1,18 +1,20 @@
-module ToolingInvoker::Setup
-  class WaitForManager
-    include Mandate
+module ToolingInvoker
+  module Setup
+    class WaitForManager
+      include Mandate
 
-    def call
-      dir = "/home/exercism"
-      return unless Dir.exist?(dir)
+      def call
+        dir = "/home/exercism"
+        return unless Dir.exist?(dir)
 
-      loop do
-        if File.exist?("#{dir}/.tooling-manager-ready")
-          Log.("Tooling Manager Ready. Continuing...")
-          return
-        else
-          Log.("Waiting for tooling manager...")
-          sleep(3)
+        loop do
+          if File.exist?("#{dir}/.tooling-manager-ready")
+            Log.("Tooling Manager Ready. Continuing...")
+            return
+          else
+            Log.("Waiting for tooling manager...")
+            sleep(3)
+          end
         end
       end
     end
