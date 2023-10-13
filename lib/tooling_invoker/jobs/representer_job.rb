522af3d4f810416cb0ed4d44d617791845ffaf1b
Erik Schierboom <erik_schierboom@hotmail.com>
2023-10-13 12:26:53 +0200

    Use endless methods for jobs

--- lib/tooling_invoker/jobs/representer_job.rb
+++ lib/tooling_invoker/jobs/representer_job.rb
@@ -1,27 +1,10 @@
 module ToolingInvoker::Jobs
   class RepresenterJob < Job
-    def type
-      "representer"
-    end
-
-    def cmd
-      "bin/run.sh"
-    end
-
-    def invocation_args
-      [exercise, "/mnt/exercism-iteration/", "/mnt/exercism-iteration/"]
-    end
-
-    def output_filepaths
-      ["representation.txt", "representation.json", "mapping.json"]
-    end
-
-    def working_directory
-      "/opt/representer"
-    end
-
-    def tool
-      "#{language}-representer"
-    end
+    def type = "representer"
+    def cmd = "bin/run.sh"
+    def invocation_args = [exercise, "/mnt/exercism-iteration/", "/mnt/exercism-iteration/"]
+    def output_filepaths = ["representation.txt", "representation.json", "mapping.json"]
+    def working_directory = "/opt/representer"
+    def tool = "#{language}-representer"
   end
 end
--- lib/tooling_invoker/jobs/test_runner_job.rb
+++ lib/tooling_invoker/jobs/test_runner_job.rb
@@ -1,25 +1,11 @@
 module ToolingInvoker
   module Jobs
     class TestRunnerJob < Job
-      def type
-        "test-runner"
-      end
-
-      def cmd
-        "bin/run.sh"
-      end
-
-      def invocation_args
-        [exercise, "/mnt/exercism-iteration/", "/mnt/exercism-iteration/"]
-      end
-
-      def output_filepaths
-        ["results.json"]
-      end
-
-      def working_directory
-        "/opt/test-runner"
-      end
+      def type = "test-runner"
+      def cmd = "bin/run.sh"
+      def invocation_args = [exercise, "/mnt/exercism-iteration/", "/mnt/exercism-iteration/"]
+      def output_filepaths = ["results.json"]
+      def working_directory = "/opt/test-runner"
     end
   end
 end
