b7bbbdc9a07901b73f7e14a31bb610ca334112a7
Jeremy Walker <jez.walker@gmail.com>
2025-06-22 22:23:41 +0900

    Fix canary

--- lib/tooling_invoker/worker/check_canary.rb
+++ lib/tooling_invoker/worker/check_canary.rb
@@ -22,7 +22,7 @@ module ToolingInvoker
       end
 
       CANARY_SOURCE = {
-        'submission_efs_root' => "cb5a174a13494e3a8aa556bc5097b7e2",
+        'submission_efs_root' => "/mnt/efs/tooling_jobs/canary",
         'submission_filepaths' => ["hello_world.rb"],
         'exercise_git_repo' => "ruby",
         'exercise_git_sha' => "508219b5722e3d5b678299159ceb396349cc0b25",
